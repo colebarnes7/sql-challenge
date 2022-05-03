5c121e282bfc76b59b95b4bfb981baa206aa7654
Cole Barnes <colebarnes7@gmail.com>
2022-05-03 13:54:23 -0500

    Started Step 4 and completed step 5

--- employeeSQL/queries.sql
+++ employeeSQL/queries.sql
@@ -1,10 +1,10 @@
 --Checking that all tables display appropriately
-SELECT * FROM departments
-SELECT * FROM titles
-SELECT * FROM employees
-SELECT * FROM salaries
-SELECT * FROM dept_employee
-SELECT * FROM dept_manager
+SELECT * FROM departments;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM dept_employee;
+SELECT * FROM dept_manager;
 
 --Step 1: list employee number, last name, first name,
 --sex and salary for each employee
@@ -34,3 +34,23 @@ emp_num,
 	FROM employees e
 	WHERE dm.emp_num = e.emp_num)
 FROM dept_manager dm;
+
+--Step 4: list department of each employee with following info:
+--employee number, last name, first name and department name
+SELECT emp_num, last_name, first_name,
+(SELECT dept_name
+	FROM departments d
+	WHERE dept_num IN
+	(
+		SELECT dept_num
+		FROM dept_employee de
+		WHERE de.emp_num = e.emp_num)
+)
+FROM employees e;
+
+--Step 5: list first name, last name and sex for employees whose
+--first name is "Hercules" and last name starts with "B"
+SELECT first_name, last_name, sex
+FROM employees e
+WHERE e.first_name = 'Hercules'
+AND e.last_name LIKE 'B%';
